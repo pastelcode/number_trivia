fc6bf6e59a98b29a16927c73f59e931d0d5dff83
Pastel <pastel.dev.gt@gmail.com>
2022-09-04 13:45:21 -0600

    Add theme bloc to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,70 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:number_trivia/core/presentation/bloc/theme/theme_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const NumberTriviaApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // <- Set up Hydrated Bloc
+  final storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  // ->
+  HydratedBlocOverrides.runZoned(
+    () => runApp(const _NumberTriviaApp()),
+    storage: storage,
+  );
 }
 
-/// {@template number_trivia_app}
-/// Root entry class for application.
+/// {@template number_trivia_app_state_provider}
+/// Root entry class for application that creates all core blocs.
 /// {@endtemplate}
 class NumberTriviaApp extends StatelessWidget {
-  /// {@macro number_trivia_app}
+  /// {@macro number_trivia_app_state_provider}
   const NumberTriviaApp({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Number trivia',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SizedBox(),
+    return MultiBlocProvider(
+      providers: <BlocProvider>[
+        BlocProvider(
+          create: (BuildContext context) => ThemeBloc(),
+        ),
+      ],
+      child: const _NumberTriviaApp(),
+    );
+  }
+}
+
+/// {@template number_trivia_app}
+/// Root entry class for application UI.
+/// {@endtemplate}
+class _NumberTriviaApp extends StatelessWidget {
+  /// {@macro number_trivia_app}
+  const _NumberTriviaApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeBloc, ThemeMode>(
+      builder: (
+        BuildContext context,
+        ThemeMode state,
+      ) {
+        return MaterialApp(
+          title: 'Number trivia',
+          themeMode: state,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const SizedBox(),
+        );
+      },
     );
   }
 }
