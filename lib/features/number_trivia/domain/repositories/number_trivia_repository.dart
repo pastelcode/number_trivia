3966a7efead4fe80ef8c71d0e4c3e9594364f781
Pastel <pastel.dev.gt@gmail.com>
2022-09-04 15:07:04 -0600

    Create interface for number trivia repository

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,11 @@
+part of 'repositories.dart';
+
+/// {@template number_trivia_repository}
+/// Interface for number trivia repository.
+/// {@endtemplate}
+mixin NumberTriviaRepository {
+  /// Gets a fact about [number].
+  Future<Either<Failure, NumberTrivia>> getNumberTrivia({
+    required int number,
+  });
+}
---
+++ lib/features/number_trivia/domain/repositories/repositories.dart
@@ -0,0 +1,5 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/errors/errors.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/entities.dart';
+
+part 'number_trivia_repository.dart';
