a032a9581dfd015d6e324040a6eb5f0f4c8fb40f
Pastel <pastel.dev.gt@gmail.com>
2022-09-04 14:59:39 -0600

    Create interface for failures

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,9 @@
+part of 'errors.dart';
+
+/// {@template failure}
+/// Interface to represent a failure when fetching data from repositories.
+/// {@endtemplate}
+abstract class Failure extends Equatable {
+  /// {@macro failure}
+  const Failure();
+}
