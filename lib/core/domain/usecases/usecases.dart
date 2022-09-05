cbf6539c5bd62c3b079ce047920b7901e28dc4a7
Pastel <pastel.dev.gt@gmail.com>
2022-09-04 23:15:09 -0600

    Update use case interface

--- lib/core/domain/usecases/usecases.dart
+++ lib/core/domain/usecases/usecases.dart
@@ -4,12 +4,14 @@ import 'package:number_trivia/core/errors/errors.dart';
 /// {@template usecase}
 /// Interface that contains common methods for use cases.
 /// {@endtemplate}
-mixin Usecase<Entity> {
+mixin Usecase<Entity, Params> {
   /// This function makes possible to use the usecase class as a callable class.
   /// Example:
   /// ```dart
   /// final MyUsecase myUsecase = MyUsecase();
   /// final result = myUsecase();
   /// ```
-  Future<Either<Failure, Entity>> call();
+  Future<Either<Failure, Entity>> call({
+    required Params params,
+  });
 }
