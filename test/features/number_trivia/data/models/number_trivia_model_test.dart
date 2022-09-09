583aa256b47cbe33fbee54167bace176ea48448b
Pastel <pastel.dev.gt@gmail.com>
2022-09-08 19:54:16 -0600

    Fix problem

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -46,7 +46,8 @@ void main() {
         },
       );
       test(
-        'should return a valid model when the JSON number is regarded as a double',
+        '''
+should return a valid model when the JSON number is regarded as a double''',
         () async {
           // arrange
           final jsonMap = fixture(
