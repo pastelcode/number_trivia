806400a88363a3fa544b51cdcceed4812184f63b
Pastel <pastel.dev.gt@gmail.com>
2022-10-07 12:25:22 -0600

    Update theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -5,18 +5,33 @@ mixin ApplicationTheme {
   /// The primary color of the application.
   static Color get primaryColor => Colors.green;
 
+  /// Whether application should use Material3 design system.
+  static bool get useMaterial3 => true;
+
+  static AppBarTheme get _appBarTheme => const AppBarTheme(
+        centerTitle: true,
+      );
+
+  static ColorScheme get _lightColorScheme => ColorScheme.fromSeed(
+        seedColor: primaryColor,
+      );
+
+  static ColorScheme get _darkColorScheme => ColorScheme.fromSeed(
+        seedColor: primaryColor,
+        brightness: Brightness.dark,
+      );
+
   /// The light [ThemeData] to pass to a [MaterialApp].
   static ThemeData get lightTheme => ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: primaryColor,
-        ),
+        useMaterial3: useMaterial3,
+        appBarTheme: _appBarTheme,
+        colorScheme: _lightColorScheme,
       );
 
   /// The dark [ThemeData] to pass to a [MaterialApp].
   static ThemeData get darkTheme => ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: primaryColor,
-          brightness: Brightness.dark,
-        ),
+        useMaterial3: useMaterial3,
+        appBarTheme: _appBarTheme,
+        colorScheme: _darkColorScheme,
       );
 }
