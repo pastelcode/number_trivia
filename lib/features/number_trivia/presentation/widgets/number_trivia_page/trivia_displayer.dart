11d8a2fb3b3d23e4257e4ca25d704a8d1c5d7975
Pastel <pastel.dev.gt@gmail.com>
2022-10-08 20:20:48 -0600

    Add animation to number trivia number

--- lib/features/number_trivia/presentation/widgets/number_trivia_page/trivia_displayer.dart
+++ lib/features/number_trivia/presentation/widgets/number_trivia_page/trivia_displayer.dart
@@ -47,12 +47,8 @@ class _Content extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(
-          '${trivia.number}',
-          style: theme.textTheme.headline2!.copyWith(
-            fontSize: 64,
-            color: theme.colorScheme.onSurface,
-          ),
+        _Number(
+          number: trivia.number,
         ),
         const SizedBox(
           height: 20,
@@ -62,6 +58,64 @@ class _Content extends StatelessWidget {
           style: theme.textTheme.bodyLarge,
         ),
       ],
+    );
+  }
+}
+
+class _Number extends StatefulWidget {
+  const _Number({
+    required this.number,
+  });
+
+  final int number;
+
+  @override
+  State<_Number> createState() => __NumberState();
+}
+
+class __NumberState extends State<_Number> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(
+        seconds: 2,
+      ),
+      vsync: this,
+    )..forward();
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    final theme = Theme.of(
+      context,
+    );
+
+    return RotationTransition(
+      turns: _animation,
+      child: Text(
+        '${widget.number}',
+        style: theme.textTheme.headline2!.copyWith(
+          fontSize: 64,
+          color: theme.colorScheme.onSurface,
+        ),
+      ),
     );
   }
 }
